97db6ebd8a69b3b8899dba2a7de2bf0fbebe069b
Lukas Waymann <meribold@gmail.com>
2014-11-10 19:21:08 +0000

    First commit

---
+++ PerCharacterFavoriteMounts.lua
@@ -0,0 +1,56 @@
+local addOnName, addOn = ...
+
+addOn._G = _G
+setfenv(1, addOn)
+
+local C_MountJournal = _G.C_MountJournal
+
+local eventHandler = _G.CreateFrame("Frame")
+eventHandler:SetScript("OnEvent", function(_, event, ...)
+  return addOn[event](addOn, ...)
+end)
+
+function addOn:ADDON_LOADED(name)
+  --_G.assert(name == addOnName)
+  eventHandler:UnregisterEvent("ADDON_LOADED")
+  _G.FavoriteMounts = _G.FavoriteMounts or {}
+  eventHandler:RegisterEvent("PLAYER_LOGIN")
+  self.ADDON_LOADED = nil
+end
+
+function addOn:PLAYER_LOGIN()
+  --_G.assert(C_MountJournal)
+
+  -- Restore favorite mounts.
+  for i = 1, C_MountJournal.GetNumMounts() do
+    local isFavorite, canFavorite = C_MountJournal.GetIsFavorite(i)
+    local _, spellID, _, _, _, _, _, _, _, hideOnChar, isCollected = C_MountJournal.GetMountInfo(i)
+    local shouldFavorite = _G.FavoriteMounts[spellID] or false
+    if isFavorite ~= shouldFavorite then
+      C_MountJournal.SetIsFavorite(i, shouldFavorite)
+    end
+  end
+
+  -- Save favorite mounts as they are added.
+  _G.hooksecurefunc(C_MountJournal, "SetIsFavorite", function(index, value)
+    --_G.print("SetIsFavorite(" .. index .. ", " .. _G.tostring(value) .. ")")
+    local _, spellID = C_MountJournal.GetMountInfo(index)
+    _G.FavoriteMounts[spellID] = value or nil -- Assign nil (remove the key) when value == false.
+  end)
+
+  self.PLAYER_LOGIN = nil
+end
+
+eventHandler:RegisterEvent("ADDON_LOADED")
+
+--[[
+Links to webpages that were useful.
+  https://github.com/Adirelle/Squire3/blob/master/Squire3.lua
+  http://wowpedia.org/API_C_MountJournal.GetIsFavorite
+  http://wowpedia.org/API_C_MountJournal.SetIsFavorite
+  http://wowpedia.org/API_C_MountJournal.GetNumMounts
+  http://wowpedia.org/API_C_MountJournal.GetMountInfo
+  http://wowpedia.org/API_C_MountJournal.GetMountInfoExtra
+]]
+
+-- vim: tw=120 sw=2 et
