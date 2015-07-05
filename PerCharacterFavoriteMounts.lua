af1f2aebac200708d877601c09ee5b1be05291cd
Lukas Waymann <meribold@gmail.com>
2015-07-05 16:47:55 +0000

    Work around the API being dodgy
    Apparently some mounts can't be (un)favorited immediately in response to
    ADDON_LOADED.  The restoreFavoriteMounts() function will now continue to
    recurse every 0.5 seconds until all mounts' favorite statuses have been
    restored.

--- PerCharacterFavoriteMounts.lua
+++ PerCharacterFavoriteMounts.lua
@@ -1,7 +1,11 @@
-local addOnName, addOn = ...
+local addonName, addon = ...
 
-addOn._G = _G
-setfenv(1, addOn)
+addon._G = _G
+setfenv(1, addon)
+
+print = function(...)
+  _G.print("|cffff7d0a" .. addonName .. "|r:", ...)
+end
 
 local C_MountJournal = _G.C_MountJournal
 
@@ -9,25 +13,47 @@ function restoreFavoriteMounts()
   for i = 1, C_MountJournal.GetNumMounts() do
     local isFavorite, canFavorite = C_MountJournal.GetIsFavorite(i)
     if canFavorite then
-      local _, spellID, _, _, _, _, _, _, _, hideOnChar, isCollected = C_MountJournal.GetMountInfo(i)
+      local creatureName, spellId, _, _, _, _, _, _, _, hideOnChar, isCollected = C_MountJournal.GetMountInfo(i)
       if not hideOnChar and isCollected then -- Weird things happen when we try to (un)favorite hidden mounts.
-        local shouldFavorite = _G.FavoriteMounts[spellID] or false
+        local shouldFavorite = _G.FavoriteMounts[spellId] or false
         if isFavorite ~= shouldFavorite then
-          --_G.print(i, spellID, _G.GetSpellInfo(spellID), shouldFavorite)
           C_MountJournal.SetIsFavorite(i, shouldFavorite)
+          --[[
+          if shouldFavorite then
+            print("Favoriting \"" .. creatureName .. "\" (" .. spellId .. ")")
+          else
+            print("Unfavoriting \"" .. creatureName .. "\" (" .. spellId .. ")")
+          end
+          --]]
+        end
+      end
+    end
+  end
+  for i = 1, C_MountJournal.GetNumMounts() do
+    local isFavorite, canFavorite = C_MountJournal.GetIsFavorite(i)
+    if canFavorite then
+      local _, spellId, _, _, _, _, _, _, _, hideOnChar, isCollected = C_MountJournal.GetMountInfo(i)
+      if not hideOnChar and isCollected then
+        local shouldFavorite = _G.FavoriteMounts[spellId] or false
+        if isFavorite ~= shouldFavorite then
+          --print("Trying again in 0.5 seconds...")
+          _G.C_Timer.After(.5, restoreFavoriteMounts)
+          break
         end
       end
     end
   end
 end
 
+--_G.restoreFavoriteMounts = restoreFavoriteMounts
+
 local eventHandler = _G.CreateFrame("Frame")
 eventHandler:SetScript("OnEvent", function(_, event, ...)
-  return addOn[event](addOn, ...)
+  return addon[event](addon, ...)
 end)
 
-function addOn:ADDON_LOADED(name)
-  if name ~= addOnName then return end
+function addon:ADDON_LOADED(name)
+  if name ~= addonName then return end
 
   eventHandler:UnregisterEvent("ADDON_LOADED")
 
@@ -37,7 +63,7 @@ function addOn:ADDON_LOADED(name)
   self.ADDON_LOADED = nil
 end
 
-function addOn:PLAYER_ENTERING_WORLD()
+function addon:PLAYER_ENTERING_WORLD()
   --_G.assert(C_MountJournal)
   eventHandler:UnregisterEvent("PLAYER_ENTERING_WORLD")
 
@@ -45,9 +71,9 @@ function addOn:PLAYER_ENTERING_WORLD()
 
   -- Save favorite mounts as they are added.
   _G.hooksecurefunc(C_MountJournal, "SetIsFavorite", function(index, value)
-    --_G.print("SetIsFavorite(" .. index .. ", " .. _G.tostring(value) .. ")")
-    local _, spellID = C_MountJournal.GetMountInfo(index)
-    _G.FavoriteMounts[spellID] = value or nil -- Assign nil (remove the key) when value == false.
+    --print("SetIsFavorite(" .. index .. ", " .. _G.tostring(value) .. ")")
+    local _, spellId = C_MountJournal.GetMountInfo(index)
+    _G.FavoriteMounts[spellId] = value or nil -- Assign nil (remove the key) when value == false.
   end)
 
   self.PLAYER_ENTERING_WORLD = nil
@@ -65,4 +91,4 @@ Links to webpages that were useful.
   http://wowpedia.org/API_C_MountJournal.GetMountInfoExtra
 ]]
 
--- vim: tw=120 sw=2 et
+-- vim: tw=120 sts=2 sw=2 et
